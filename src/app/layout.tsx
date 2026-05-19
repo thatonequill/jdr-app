dd1c93cb930a7cdd7c95828832cc6fd3b3d1b929
Quill <thatonequill@gmail.com>
2026-05-19 15:50:51 +0200

    Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,13 +30,13 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: './favicon.ico' },
-      { url: './favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: './favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon.ico' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
     ],
-    apple: { url: './apple-touch-icon.png', sizes: '180x180' },
+    apple: { url: '/apple-touch-icon.png', sizes: '180x180' },
   },
-  manifest: './site.webmanifest',
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({
